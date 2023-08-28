65c4c3f2562833b38a22af31bb87017c2ea84294
garcia2697 <luisgarcia.lg37@gmail.com>
2023-08-27 22:44:50 -0500

    starts function off a button press

--- deck.js
+++ deck.js
@@ -41,10 +41,17 @@ export default class Deck{
 
         }
     }
+    GFG_Fun() {
+        const cardDiv = document.createElement('img')
+        cardDiv.src = stackOfCards
+        document.getElementById('body').appendChild(stackOfCards[0].imgagePath);
+        res.innerHTML = "Image Element Added."
+        console.log("TEST")
+    }
 }
 
 class Card {
-    constructor(SUITS, VALUES,COLOR){
+    constructor(SUITS, VALUES){
         this.SUITS = SUITS
         this.VALUES = VALUES
         
@@ -60,64 +67,64 @@ function freshDeck(){
 }
 
 const  stackOfCards = [
-    {id:1, imgagePath:'/playing-cards-assets/2_of_clubs.png',cardValue:2,cardColor:"black", suite: "clubs"},
-    {id:2, imgagePath:'/playing-cards-assets/3_of_clubs.png',cardValue:3,cardColor:"black",suite: "clubs"},
-    {id:3, imgagePath:'/playing-cards-assets/4_of_clubs.png',cardValue:4,cardColor:"black",suite: "clubs"},
-    {id:4, imgagePath:'/playing-cards-assets/5_of_clubs.png',cardValue:5,cardColor:"black",suite: "clubs"},
-    {id:5, imgagePath:'/playing-cards-assets/6_of_clubs.png',cardValue:6,cardColor:"black",suite: "clubs"},
-    {id:6, imgagePath:'/playing-cards-assets/7_of_clubs.png',cardValue:7,cardColor:"black" ,suite: "clubs" },
-    {id:7, imgagePath:'/playing-cards-assets/7_of_clubs.png',cardValue:8,cardColor:"black",suite: "clubs"},
-    {id:8, imgagePath:'/playing-cards-assets/8_of_clubs.png',cardValue:9,cardColor:"black",suite: "clubs"},
-    {id:9, imgagePath:'/playing-cards-assets/9_of_clubs.png',cardValue:10,cardColor:"black",suite: "clubs"},
-    {id:10, imgagePath:'/playing-cards-assets/jack_of_clubs.png',cardValue:11,cardColor:"black",suite: "clubs"},
-    {id:11, imgagePath:'/playing-cards-assets/queen_of_clubs.png',cardValue:12,cardColor:"black",suite: "clubs"},
-    {id:12, imgagePath:'/playing-cards-assets/king_of_clubs.png',cardValue:13,cardColor:"black",suite: "clubs"},
-    {id:13, imgagePath:'/playing-cards-assets/ace_of_clubs.png',cardValue:14,cardColor:"black",suite: "clubs"},
+    {id:1, imgagePath:'/playing-cards-assets/png/2_of_clubs.png',cardValue:2,cardColor:"black", suit: "clubs"},
+    {id:2, imgagePath:'/playing-cards-assets/png/3_of_clubs.png',cardValue:3,cardColor:"black",suit: "clubs"},
+    {id:3, imgagePath:'/playing-cards-assets/png/4_of_clubs.png',cardValue:4,cardColor:"black",suit: "clubs"},
+    {id:4, imgagePath:'/playing-cards-assets/png/5_of_clubs.png',cardValue:5,cardColor:"black",suit: "clubs"},
+    {id:5, imgagePath:'/playing-cards-assets/png/6_of_clubs.png',cardValue:6,cardColor:"black",suit: "clubs"},
+    {id:6, imgagePath:'/playing-cards-assets/png/7_of_clubs.png',cardValue:7,cardColor:"black" ,suit: "clubs" },
+    {id:7, imgagePath:'/playing-cards-assets/png/7_of_clubs.png',cardValue:8,cardColor:"black",suit: "clubs"},
+    {id:8, imgagePath:'/playing-cards-assets/png/8_of_clubs.png',cardValue:9,cardColor:"black",suit: "clubs"},
+    {id:9, imgagePath:'/playing-cards-assets/png/9_of_clubs.png',cardValue:10,cardColor:"black",suit: "clubs"},
+    {id:10, imgagePath:'/playing-cards-assets/png/jack_of_clubs.png',cardValue:11,cardColor:"black",suit: "clubs"},
+    {id:11, imgagePath:'/playing-cards-assets/png/queen_of_clubs.png',cardValue:12,cardColor:"black",suit: "clubs"},
+    {id:12, imgagePath:'/playing-cards-assets/png/king_of_clubs.png',cardValue:13,cardColor:"black",suit: "clubs"},
+    {id:13, imgagePath:'/playing-cards-assets/png/ace_of_clubs.png',cardValue:14,cardColor:"black",suit: "clubs"},
 
-    {id:14, imgagePath:'/playing-cards-assets/2_of_diamonds.png',cardValue:2,cardColor:"red",suite: "diamonds"},
-    {id:15, imgagePath:'/playing-cards-assets/3_of_diamonds.png',cardValue:3,cardColor:"red",suite: "diamonds"},
-    {id:16, imgagePath:'/playing-cards-assets/4_of_diamonds.png',cardValue:4,cardColor:"red",suite: "diamonds"},
-    {id:17, imgagePath:'/playing-cards-assets/5_of_diamonds.png',cardValue:5,cardColor:"red",suite: "diamonds"},
-    {id:18, imgagePath:'/playing-cards-assets/6_of_diamonds.png',cardValue:6,cardColor:"red",suite: "diamonds"},
-    {id:19, imgagePath:'/playing-cards-assets/7_of_diamonds.png',cardValue:7,cardColor:"red",suite: "diamonds"},
-    {id:20, imgagePath:'/playing-cards-assets/7_of_diamonds.png',cardValue:8,cardColor:"red",suite: "diamonds"},
-    {id:21, imgagePath:'/playing-cards-assets/8_of_diamonds.png',cardValue:9,cardColor:"red",suite: "diamonds"},
-    {id:22, imgagePath:'/playing-cards-assets/9_of_diamonds.png',cardValue:10,cardColor:"red",suite: "diamonds"},
-    {id:23, imgagePath:'/playing-cards-assets/jack_of_diamonds.png',cardValue:11,cardColor:"red",suite: "diamonds"},
-    {id:24, imgagePath:'/playing-cards-assets/queen_of_diamonds.png',cardValue:12,cardColor:"red",suite: "diamonds"},
-    {id:25, imgagePath:'/playing-cards-assets/king_of_diamonds.png',cardValue:13,cardColor:"red",suite: "diamonds"},
-    {id:26, imgagePath:'/playing-cards-assets/ace_of_diamonds.png',cardValue:14,cardColor:"red",suite: "diamonds"},
+    {id:14, imgagePath:'/playing-cards-assets/png/2_of_diamonds.png',cardValue:2,cardColor:"red",suit: "diamonds"},
+    {id:15, imgagePath:'/playing-cards-assets/png/3_of_diamonds.png',cardValue:3,cardColor:"red",suit: "diamonds"},
+    {id:16, imgagePath:'/playing-cards-assets/png/4_of_diamonds.png',cardValue:4,cardColor:"red",suit: "diamonds"},
+    {id:17, imgagePath:'/playing-cards-assets/png/5_of_diamonds.png',cardValue:5,cardColor:"red",suit: "diamonds"},
+    {id:18, imgagePath:'/playing-cards-assets/png/6_of_diamonds.png',cardValue:6,cardColor:"red",suit: "diamonds"},
+    {id:19, imgagePath:'/playing-cards-assets/png/7_of_diamonds.png',cardValue:7,cardColor:"red",suit: "diamonds"},
+    {id:20, imgagePath:'/playing-cards-assets/png/7_of_diamonds.png',cardValue:8,cardColor:"red",suit: "diamonds"},
+    {id:21, imgagePath:'/playing-cards-assets/png/8_of_diamonds.png',cardValue:9,cardColor:"red",suit: "diamonds"},
+    {id:22, imgagePath:'/playing-cards-assets/png/9_of_diamonds.png',cardValue:10,cardColor:"red",suit: "diamonds"},
+    {id:23, imgagePath:'/playing-cards-assets/png/jack_of_diamonds.png',cardValue:11,cardColor:"red",suit: "diamonds"},
+    {id:24, imgagePath:'/playing-cards-assets/png/queen_of_diamonds.png',cardValue:12,cardColor:"red",suit: "diamonds"},
+    {id:25, imgagePath:'/playing-cards-assets/png/king_of_diamonds.png',cardValue:13,cardColor:"red",suit: "diamonds"},
+    {id:26, imgagePath:'/playing-cards-assets/png/ace_of_diamonds.png',cardValue:14,cardColor:"red",suit: "diamonds"},
 
-    {id:27, imgagePath:'/playing-cards-assets/2_of_hearts.png',cardValue:2,cardColor:"red",suite: "hearts"},
-    {id:28, imgagePath:'/playing-cards-assets/3_of_hearts.png',cardValue:3,cardColor:"red",suite: "hearts"},
-    {id:29, imgagePath:'/playing-cards-assets/4_of_hearts.png',cardValue:4,cardColor:"red",suite: "hearts"},
-    {id:30, imgagePath:'/playing-cards-assets/5_of_hearts.png',cardValue:5,cardColor:"red",suite: "hearts"},
-    {id:31, imgagePath:'/playing-cards-assets/6_of_hearts.png',cardValue:6,cardColor:"red",suite: "hearts"},
-    {id:32, imgagePath:'/playing-cards-assets/7_of_hearts.png',cardValue:7,cardColor:"red",suite: "hearts"},
-    {id:33, imgagePath:'/playing-cards-assets/7_of_hearts.png',cardValue:8,cardColor:"red",suite: "hearts"},
-    {id:34, imgagePath:'/playing-cards-assets/8_of_hearts.png',cardValue:9,cardColor:"red",suite: "hearts"},
-    {id:35, imgagePath:'/playing-cards-assets/9_of_hearts.png',cardValue:10,cardColor:"red",suite: "hearts"},
-    {id:36, imgagePath:'/playing-cards-assets/jack_of_hearts.png',cardValue:11,cardColor:"red",suite: "hearts"},
-    {id:37, imgagePath:'/playing-cards-assets/queen_of_hearts.png',cardValue:12,cardColor:"red",suite: "hearts"},
-    {id:38, imgagePath:'/playing-cards-assets/king_of_hearts.png',cardValue:13,cardColor:"red",suite: "hearts"},
-    {id:39, imgagePath:'/playing-cards-assets/ace_of_hearts.png',cardValue:14,cardColor:"red",suite: "hearts"},
+    {id:27, imgagePath:'/playing-cards-assets/png/2_of_hearts.png',cardValue:2,cardColor:"red",suit: "hearts"},
+    {id:28, imgagePath:'/playing-cards-assets/png/3_of_hearts.png',cardValue:3,cardColor:"red",suit: "hearts"},
+    {id:29, imgagePath:'/playing-cards-assets/png/4_of_hearts.png',cardValue:4,cardColor:"red",suit: "hearts"},
+    {id:30, imgagePath:'/playing-cards-assets/png/5_of_hearts.png',cardValue:5,cardColor:"red",suit: "hearts"},
+    {id:31, imgagePath:'/playing-cards-assets/png/6_of_hearts.png',cardValue:6,cardColor:"red",suit: "hearts"},
+    {id:32, imgagePath:'/playing-cards-assets/png/7_of_hearts.png',cardValue:7,cardColor:"red",suit: "hearts"},
+    {id:33, imgagePath:'/playing-cards-assets/png/7_of_hearts.png',cardValue:8,cardColor:"red",suit: "hearts"},
+    {id:34, imgagePath:'/playing-cards-assets/png/8_of_hearts.png',cardValue:9,cardColor:"red",suit: "hearts"},
+    {id:35, imgagePath:'/playing-cards-assets/png/9_of_hearts.png',cardValue:10,cardColor:"red",suit: "hearts"},
+    {id:36, imgagePath:'/playing-cards-assets/png/jack_of_hearts.png',cardValue:11,cardColor:"red",suit: "hearts"},
+    {id:37, imgagePath:'/playing-cards-assets/png/queen_of_hearts.png',cardValue:12,cardColor:"red",suit: "hearts"},
+    {id:38, imgagePath:'/playing-cards-assets/png/king_of_hearts.png',cardValue:13,cardColor:"red",suit: "hearts"},
+    {id:39, imgagePath:'/playing-cards-assets/png/ace_of_hearts.png',cardValue:14,cardColor:"red",suit: "hearts"},
 
-    {id:40, imgagePath:'/playing-cards-assets/2_of_spades.png',cardValue:2,cardColor:"black",suite: "spades"},
-    {id:41, imgagePath:'/playing-cards-assets/3_of_spades.png',cardValue:3,cardColor:"black",suite: "spades"},
-    {id:42, imgagePath:'/playing-cards-assets/4_of_spades.png',cardValue:4,cardColor:"black",suite: "spades"},
-    {id:43, imgagePath:'/playing-cards-assets/5_of_spades.png',cardValue:5,cardColor:"black",suite: "spades"},
-    {id:44, imgagePath:'/playing-cards-assets/6_of_spades.png',cardValue:6,cardColor:"black",suite: "spades"},
-    {id:45, imgagePath:'/playing-cards-assets/7_of_spades.png',cardValue:7,cardColor:"black",suite: "spades"},
-    {id:46, imgagePath:'/playing-cards-assets/7_of_spades.png',cardValue:8,cardColor:"black",suite: "spades"},
-    {id:47, imgagePath:'/playing-cards-assets/8_of_spades.png',cardValue:9,cardColor:"black",suite: "spades"},
-    {id:48, imgagePath:'/playing-cards-assets/9_of_spades.png',cardValue:10,cardColor:"black",suite: "spades"},
-    {id:49, imgagePath:'/playing-cards-assets/jack_of_spades.png',cardValue:11,cardColor:"black",suite: "spades"},
-    {id:50, imgagePath:'/playing-cards-assets/queen_of_spades.png',cardValue:12,cardColor:"black",suite: "spades"},
-    {id:51, imgagePath:'/playing-cards-assets/king_of_spades.png',cardValue:13,cardColor:"black",suite: "spades"},
-    {id:52, imgagePath:'/playing-cards-assets/ace_of_spades.png',cardValue:14,cardColor:"black",suite: "spades"},
+    {id:40, imgagePath:'/playing-cards-assets/png/2_of_spades.png',cardValue:2,cardColor:"black",suit: "spades"},
+    {id:41, imgagePath:'/playing-cards-assets/png/3_of_spades.png',cardValue:3,cardColor:"black",suit: "spades"},
+    {id:42, imgagePath:'/playing-cards-assets/png/4_of_spades.png',cardValue:4,cardColor:"black",suit: "spades"},
+    {id:43, imgagePath:'/playing-cards-assets/png/5_of_spades.png',cardValue:5,cardColor:"black",suit: "spades"},
+    {id:44, imgagePath:'/playing-cards-assets/png/6_of_spades.png',cardValue:6,cardColor:"black",suit: "spades"},
+    {id:45, imgagePath:'/playing-cards-assets/png/7_of_spades.png',cardValue:7,cardColor:"black",suit: "spades"},
+    {id:46, imgagePath:'/playing-cards-assets/png/7_of_spades.png',cardValue:8,cardColor:"black",suit: "spades"},
+    {id:47, imgagePath:'/playing-cards-assets/png/8_of_spades.png',cardValue:9,cardColor:"black",suit: "spades"},
+    {id:48, imgagePath:'/playing-cards-assets/png/9_of_spades.png',cardValue:10,cardColor:"black",suit: "spades"},
+    {id:49, imgagePath:'/playing-cards-assets/png/jack_of_spades.png',cardValue:11,cardColor:"black",suit: "spades"},
+    {id:50, imgagePath:'/playing-cards-assets/png/queen_of_spades.png',cardValue:12,cardColor:"black",suit: "spades"},
+    {id:51, imgagePath:'/playing-cards-assets/png/king_of_spades.png',cardValue:13,cardColor:"black",suit: "spades"},
+    {id:52, imgagePath:'/playing-cards-assets/png/ace_of_spades.png',cardValue:14,cardColor:"black",suit: "spades"},
 
-    {id:53, imgagePath:'/playing-cards-assets/red_joker',cardValue:0,cardColor:"red",suite: "joker"},
-    {id:54, imgagePath:'/playing-cards-assets/black_joker',cardValue:0,cardColor:"black",suite: "joker"}
+    {id:53, imgagePath:'/playing-cards-assets/png/red_joker',cardValue:0,cardColor:"red",suit: "joker"},
+    {id:54, imgagePath:'/playing-cards-assets/png/black_joker',cardValue:0,cardColor:"black",suit: "joker"}
 
 ]
 
--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-import Deck from './deck.js'
+import Deck from "./deck.js";
 
-const deck = new Deck()
-deck.shuffle()
-console.log(deck.cards)
+const element = document.getElementById("myBtn");
+element.addEventListener("click", function startGame () {
+  
+  const deck = new Deck();
+  const beforeShuffleCard = deck.cards.pop();
+
+  deck.shuffle();
+  const lastCard = deck.cards.pop();
+  console.log(lastCard);
+  document.getElementById("demo").innerHTML = lastCard.cardColor;
+  console.log(lastCard.imgagePath)
+  var image = document.getElementById("lastCard")
+  image.src = lastCard.imgagePath
+
+});
